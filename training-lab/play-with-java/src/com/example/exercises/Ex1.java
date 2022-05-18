3c413a6d6423143c80c7942cb356d295ae94f112
Vincent Ramos <vincent.j.ramos@gmail.com>
2022-05-18 16:55:15 -0400

    updated Ex1

--- training-lab/play-with-java/src/com/example/exercises/Ex1.java
+++ training-lab/play-with-java/src/com/example/exercises/Ex1.java
@@ -1,5 +1,6 @@
 package com.example.exercises;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Ex1 {
@@ -16,6 +17,11 @@ public class Ex1 {
         String s = new String();
         char x = 65;
         int j = 1;
+        int c = 1;
+        int n;
+        int k =0;
+        int m=0;
+        int a;
 
         String t = new String();
         String u = new String();
@@ -23,19 +29,38 @@ public class Ex1 {
         String w = new String();
 
 
-        char y;
+        char y = 'A';
 
         Scanner scanner = new Scanner(System.in);
 
         int h = (int) x;
-        System.out.println("Please provide a letter:");
-        t = scanner.nextLine();
-        y = t.charAt(0);
-        int n = (int) y;
-        int k = 0;
-        int a = 0;
+       do {
+           if(c==1){
 
-        int m = (y - h) * 2 + 1;
+               System.out.println("Please provide a letter:");
+               t = scanner.nextLine();
+               y = t.charAt(0);
+               n = (int) y;
+               if(n>64 && n<91){
+                   a = 0;
+
+                   m = (y - h) * 2 + 1;
+
+                   c=2;
+
+               }else {
+
+                   System.out.println("Your input must be a single capital letter A through Z");
+                   c=1;
+
+               }
+
+           }
+
+
+
+        } while (c==1);
+
 
 
 
@@ -88,6 +113,17 @@ public class Ex1 {
                 w = w.substring(0, y-h-i) + ch + w.substring(y-h + 1-i);
                 System.out.println(w);
             }
+
+
+            }
+
+        int z = t.length();
+        if(z == 1){
+            System.out.println();
+            System.out.println("Ain't it pretty!");
+        }else {
+            System.out.println("You typed more than just a letter but we used the first letter in the string as" +
+                    " it was a capital letter that could be used.");
 
         }
 
